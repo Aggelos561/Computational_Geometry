99d28ce919d075fa2411bef965996d38b2d8cefe
Aggelos <angelos561@hotmail.com>
2022-10-21 15:20:14 +0300

    Fixed Red Segments Problem

--- main.cpp
+++ main.cpp
@@ -14,14 +14,11 @@ typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef K::Point_2 Point;
 typedef CGAL::Polygon_2<K> Polygon_2;
 typedef K::Segment_2 Segment_2;
-typedef CGAL::Convex_hull_traits_adapter_2<
-    K, CGAL::Pointer_property_map<Point>::type>
-    Convex_hull_traits_2;
+typedef CGAL::Convex_hull_traits_adapter_2<K, CGAL::Pointer_property_map<Point>::type> Convex_hull_traits_2;
 
 std::vector<Point> readPoints() {
 
-  std::vector<Point> points{Point(2, 1), Point(1, 2), Point(3, 4), Point(2, 6),
-                            Point(3, 6)};
+  std::vector<Point> points{Point(2, 1), Point(1, 2), Point(3, 4), Point(2, 6), Point(3, 6)};
   return points;
 }
 
@@ -29,14 +26,13 @@ void sortPoints(std::vector<Point> &points) {
   std::sort(points.begin(), points.end(), std::greater<Point>());
 }
 
-void initializeTriangle(std::vector<Segment_2> &polygLine,
-                        std::vector<Point> &points,
-                        std::vector<Point> &remainingPoints) {
+void initializeTriangle(std::vector<Segment_2> &polygLine, std::vector<Point> &points, std::vector<Point> &remainingPoints) {
 
   for (int i = 0; i < 3; i++) {
     polygLine.push_back(Segment_2(points[i], points[i + 1]));
     remainingPoints.erase(remainingPoints.begin());
   }
+
 }
 
 std::vector<Point> getPolyLinePoints(std::vector<Segment_2> &polygLine) {
@@ -93,8 +89,7 @@ int main() {
 
     // Find Complex Hull
     CGAL::convex_hull_2(
-        indices.begin(), indices.end(), std::back_inserter(out),
-        Convex_hull_traits_2(CGAL::make_property_map(polygLinePoints)));
+        indices.begin(), indices.end(), std::back_inserter(out), Convex_hull_traits_2(CGAL::make_property_map(polygLinePoints)));
 
     std::vector<Point> convexHullPoints;
     for (int i = 0; i < out.size(); i++) {
@@ -107,51 +102,53 @@ int main() {
     // }
 
     std::vector<Segment_2> redSegments;
+
     for (int i = 0; i < convexHullPoints.size(); i++) {
 
       Segment_2 seg;
 
       if (i == convexHullPoints.size() - 1) {
         seg = Segment_2(convexHullPoints[i], convexHullPoints[0]);
-      } else {
+      } 
+      else {
         seg = Segment_2(convexHullPoints[i], convexHullPoints[i + 1]);
       }
 
       redSegments.push_back(seg);
     }
-
+    std::vector<Segment_2> convexHullSegments = redSegments;
     Point nextPoint = remainingPoints[0];
     std::cout << "New Point: " << nextPoint << std::endl;
 
-    for (int i = 0; i < redSegments.size(); i++) {
-      Segment_2 edgeSegment1 = Segment_2(nextPoint, redSegments[i].point(0));
-      Segment_2 edgeSegment2 = Segment_2(nextPoint, redSegments[i].point(1));
-      Segment_2 midSegment = Segment_2(
-          nextPoint,
-          Point(
-              ((redSegments[i].point(0).x() + redSegments[i].point(1).x()) / 2),
-              ((redSegments[i].point(0).y() + redSegments[i].point(1).y()) /
-               2)));
+    for (int i = 0; i < convexHullSegments.size(); i++) {
+
+      Segment_2 edgeSegment1 = Segment_2(nextPoint, convexHullSegments[i].point(0));
+      
+      Segment_2 edgeSegment2 = Segment_2(nextPoint, convexHullSegments[i].point(1));
+      
+      Segment_2 midSegment = Segment_2(nextPoint, Point(((convexHullSegments[i].point(0).x() + convexHullSegments[i].point(1).x()) / 2),
+                                                        ((convexHullSegments[i].point(0).y() + convexHullSegments[i].point(1).y()) /2)));
 
       Segment_2 segmentsArray[] = {edgeSegment1, edgeSegment2, midSegment};
+      Segment_2 currSegment = convexHullSegments[i];
 
-      for (int j = 0; j < redSegments.size(); j++) {
-        if (j == i)
-          continue;
+      for (int j = 0; j < convexHullSegments.size(); j++) {
+        
+        if (j == i) continue;
 
         for (int k = 0; k < 3; k++) {
-          const auto result = intersection(segmentsArray[k], redSegments[j]);
+
+          const auto result = intersection(segmentsArray[k], convexHullSegments[j]);
 
           if (result) {
 
             const Point *p = boost::get<Point>(&*result);
-            std::cout << *p << std::endl;
 
-            if (*p == redSegments[j].point(0) ||
-                *p == redSegments[j].point(1)) {
+            if (*p == convexHullSegments[j].point(0) || *p == convexHullSegments[j].point(1)) {
               continue;
-            } else {
-              redSegments.erase(redSegments.begin() + j);
+            } 
+            else {
+              redSegments.erase(std::remove(redSegments.begin(), redSegments.end(), currSegment), redSegments.end());
             }
           }
         }
